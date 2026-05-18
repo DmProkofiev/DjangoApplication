69b31937332744934f317187147b04fb6f2c2736
studentcoll <studentcoll@tp.local>
2026-05-18 21:35:47 +0300

    18.05.2026

--- django_password_generator/urls.py
+++ django_password_generator/urls.py
@@ -1,10 +1,17 @@
 
 # Маршрутизатор (urls.py)
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from vault import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('generator/', include('vault.urls')),
+    # конструкция Django, которая подключает все маршруты (URL-ы) из другого файла — а именно из vault/urls.py.
+    # Она используется для модульной организации маршрутов, когда проект разрастается и маршрутов становится много
+
+    path('user/', views.users, name='user'),
 ]
--- vault/views.py
+++ vault/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from urllib3 import request
+
 
 # Обработчик  обрабатывает запрос и возвращает ответ
 def home(request):
     return render(request, 'vault/home.html')
+
+def about(request):
+    return render(request, 'vault/about.html')
+
+def contact(request):
+    return render(request, 'vault/contact.html')
+
+def help(request):
+    return render(request, 'vault/help.html')
+
+def users(request):
+    return render(request, 'vault/user.html')
 
 # def views():
 #     return None
